f9f875e5a171f91fab478d95eb095e72c25a0d53
Rkj2304034 <rahulkumarjha2006@gmail.com>
2025-06-14 08:23:36 +0530

    last

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,5 @@
 import User from "../models/user.js"
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import { sendOtp } from "../services/sendOtp.js";
 import Verification from "../models/verification.js"
 import { genToken } from "../services/auth.js";
